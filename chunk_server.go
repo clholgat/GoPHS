b446937720b0a26256219c7c33c800f51fcc9c2e
Carson Holgate <clholgat@ncsu.edu>
2012-10-06 17:33:49 -0400

    first commit, also full of bugs

---
+++ chunk_server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"./ohhai"
+	"bufio"
+	"code.google.com/p/goprotobuf/proto"
+	"fmt"
+	"net"
+	"os"
+)
+
+var (
+	SERVER_ID = 0
+	MASTER    = ""
+)
+
+// This function runs before main
+// Handles setup with the master server to grab an ID
+func init() {
+	// Read master server from config file
+	file, err := os.Open("master.cfg")
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(file)
+	MASTER, err := reader.ReadString('\n')
+	if err != nil {
+		//panic(err)
+	}
+	fmt.Println(MASTER)
+
+	// Call master and ask for a server ID
+	conn, err := net.Dial("tcp", MASTER)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sending ping to master")
+	fmt.Fprintf(conn, "ping")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
+}
+
+func main() {
+	fmt.Println("All the things")
+
+	l, err := net.Listen("tcp", ":12346")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	buf, err := bufio.NewReader(conn).ReadBytes('\n')
+	if err != nil {
+		panic(err)
+	}
+	message := &ohhai.OhHai{}
+	err = proto.Unmarshal(buf, message)
+	message_type := message.GetMessageType()
+	switch message_type {
+	case ohhai.OhHai_HEARTBEAT_REQUEST:
+		fmt.Println("Hearbeat")
+	case ohhai.OhHai_READ_REQUEST:
+		fmt.Println("Read")
+	case ohhai.OhHai_WRITE_REQUEST:
+		fmt.Println("Write")
+	default:
+		fmt.Println("WAT?")
+	}
+}
+
+func getChunkId() int64 {
+	return int64(1)
+}
---
+++ master.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"./ohhai"
+	"bufio"
+	"code.google.com/p/goprotobuf/proto"
+	"fmt"
+	"io"
+	"net"
+)
+
+var (
+	CHUNK_SERVERS = make([]string, 0, 10)
+)
+
+func main() {
+	listen()
+	talk()
+}
+
+func listen() {
+	l, err := net.Listen("tcp", ":12345")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func(c net.Conn) {
+			fmt.Println("Running func")
+			incoming := c.LocalAddr().String()
+			fmt.Println("connecting. . .", incoming)
+			CHUNK_SERVERS = append(CHUNK_SERVERS, incoming)
+			sendHeartBeatRequest(incoming)
+			io.WriteString(c, "1")
+			c.Close()
+		}(conn)
+	}
+}
+
+func talk() {
+
+}
+
+func sendHeartBeatRequest(server string) {
+	fmt.Println("sending heartbeat request")
+	request := &ohhai.OhHai{
+		MessageType:      ohhai.OhHai_HEARTBEAT_REQUEST.Enum(),
+		HeartBeatRequest: &ohhai.OhHai_HeartBeatRequest{},
+	}
+	conn, err := net.Dial("tcp", server)
+	if err != nil {
+		panic(err)
+	}
+	writer := bufio.NewWriter(conn)
+	bytes, err := proto.Marshal(request)
+	if err != nil {
+		panic(err)
+	}
+	_, err = writer.Write(bytes)
+	if err != nil {
+		panic(err)
+	}
+	conn.Close()
+}
